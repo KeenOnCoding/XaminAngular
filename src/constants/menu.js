47966488054bf807d04d167a3466caf68530f34f
Sergei <ryabushenko.serhiy@gmail.com>
2024-06-06 10:12:19 +0300

    commit_2

--- src/constants/menu.js
+++ src/constants/menu.js
@@ -1,72 +1,8 @@
 const topMenuBarItems = [
-  {
-    href: '/landing-page1',
-    title: 'Home  ',
-    isCurrent: true,
-    submenu: [
-      {
-        href: '/landing-page1',
-        title: 'Home One'
-      },
-      {
-        href: '/landing-page2',
-        title: 'Home Two'
-      },
-      {
-        href: '/landing-page3',
-        title: 'Home Three'
-      },
-      {
-        href: '/landing-page4',
-        title: 'User Anaytics'
-      },
-      {
-        href: '/landing-page5',
-        title: 'Business Intelligence'
-      },
-      {
-        href: '/landing-page6',
-        title: 'Predective Analytics'
-      },
-      {
-        href: '/landing-page7',
-        title: 'Sentiment Analysis'
-      },
-      {
-        href: '/landing-page8',
-        title: 'User Behaviour Analysis'
-      },
-      {
-        href: '/landing-page9',
-        title: 'Data Scientist Profile'
-      },
-      {
-        href: '/landing-page10',
-        title: 'Data Visualization Service'
-      },
-      {
-        href: '/landing-page11',
-        title: 'Analytics In Marketing'
-      }
-    ]
-  },
-  {
-    href: '/about-us',
-    title: 'About Us ',
-    submenu: [
-      {
-        href: '/about-us',
-        title: 'About Us One'
-      },
-      {
-        href: '/about-us/about-us-two',
-        title: 'About Us Two'
-      }
-    ]
-  },
+
   {
     href: '/services',
-    title: 'Service',
+    title: 'Services ',
     submenu : [
       {
         href: '/services/data-analytics',
@@ -102,136 +38,231 @@ const topMenuBarItems = [
       }
     ]
   },
+
   {
     href: '/our-cases/portfolio-2-columns',
-    title: 'Our Cases',
+    title: 'Our Cases ',
     submenu: [
       {
-        href: '#portfolio',
-        title: 'Portfolio',
-        submenu:[
-          {
-            href: '/our-cases/portfolio-2-columns',
-            title: 'Portfolio 2 Columns'
+            href: '/landing-page4',
+            title: 'User Anaytics'
           },
           {
-            href: '/our-cases/portfolio-3-columns',
-            title: 'Portfolio 3 Columns'
+            href: '/landing-page5',
+            title: 'Business Intelligence'
           },
           {
-            href: '/our-cases/portfolio-4-columns',
-            title: 'Portfolio 4 Columns'
+            href: '/landing-page6',
+            title: 'Predective Analytics'
           },
           {
-            href: '/our-cases/portfolio-5-columns',
-            title: 'Portfolio 5 Columns'
+            href: '/landing-page7',
+            title: 'Sentiment Analysis'
           },
           {
-            href: '/our-cases/portfolio-no-space',
-            title: 'Portfolio No Space'
+            href: '/landing-page8',
+            title: 'User Behaviour Analysis'
+          },
+          {
+            href: '/landing-page9',
+            title: 'Data Scientist Profile'
+          },
+          {
+            href: '/landing-page10',
+            title: 'Data Visualization Service'
+          },
+          {
+            href: '/landing-page11',
+            title: 'Analytics In Marketing'
           }
-        ]
-      },
-      {
-        href: '/our-cases/masonry-2-portfolio',
-        title: 'Masonry 2 Portfolio'
-      },
-      {
-        href: '/our-cases/masonry-3-portfolio',
-        title: 'Masonry 3 Portfolio'
-      },
-      {
-        href: '/our-cases/masonry-no-space',
-        title: 'Masonry No Space'
-      },
-      {
-        href: '/our-cases/portfolio-details',
-        title: 'Portfolio Details'
-      }
-    ]
-  },
-  {
-    href: '/blog',
-    title: 'Blog',
-    submenu: [
-      {
-        href: '/blog',
-        title: 'Blog'
-      },
-      {
-        href: '#blog-grid',
-        title: 'Blog Grid',
-        submenu: [
-          {
-            href: '/blog/blog-one-column-grid',
-            title: 'One Column Blog'
-          },
-          {
-            href: '/blog/blog-two-column-grid',
-            title: 'Two Column Blog'
-          },
-          {
-            href: '/blog/blog-three-column-grid',
-            title: 'Three Column Blog',
-          }
-        ]
-      },
-      {
-        href: '/blog/blog-right-sidebar-grid-one',
-        title: 'Right SideBar Grid',
-        submenu: [
-          {
-            href: '/blog/blog-right-sidebar-grid-one',
-            title: 'Right Sidebar Grid 1'
-          },
-          {
-            href: '/blog/blog-right-sidebar-grid-two',
-            title: 'Right Sidebar Grid 2'
-          }
-        ]
-      },
-      {
-        href: '/blog/blog-left-sidebar-grid-one',
-        title: 'Left SideBar Grid',
-        submenu: [
-          {
-            href: '/blog/blog-left-sidebar-grid-one',
-            title: 'Left Sidebar Grid 1'
-          },
-          {
-            href: '/blog/blog-left-sidebar-grid-two',
-            title: 'Left Sidebar Grid 1'
-          }
-        ]
-      },
-      {
-        href: '/blog/blog-details',
-        title: 'Blog Details'
-      }
     ]
   },
   {
     href: '/pages/our-team',
-    title: 'Pages',
-    submenu: [
-      {
-        href: '/pages/our-team',
-        title: 'Our Team'
-      },
-      {
-        href: '/pages/client',
-        title: 'Clients'
-      },
-      {
-        href: '/pages/career',
-        title: 'Careers',
-      },
-      {
-        href: '/pages/contact-us',
-        title: 'Contact Us',
-      }
-    ]
+    title: 'Team'
+  },
+  {
+    href: '/pages/client',
+    title: 'Clients'
+  },
+  {
+    href: '/pages/career',
+    title: 'Careers',
   }
+  //{
+  //  href: '/',
+  //  title: 'About Us ',
+  //  submenu: [
+  //    {
+
+  //    },
+  //    {
+
+  //    },
+  //    {
+
+  //    },
+  //    {
+  //      href: '/pages/contact-us',
+  //      title: 'Contact Us',
+  //    }
+  //  ]
+  //},
+
+  //{
+  //  href: '/landing-page1',
+  //  title: 'Home  ',
+  //  isCurrent: true,
+  //  submenu: [
+  //    {
+  //      href: '/landing-page1',
+  //      title: 'Home One'
+  //    },
+  //    {
+  //      href: '/landing-page2',
+  //      title: 'Home Two'
+  //    },
+  //    {
+  //      href: '/landing-page3',
+  //      title: 'Home Three'
+  //    },
+  //    {
+  //      href: '/landing-page4',
+  //      title: 'User Anaytics'
+  //    },
+  //    {
+  //      href: '/landing-page5',
+  //      title: 'Business Intelligence'
+  //    },
+  //    {
+  //      href: '/landing-page6',
+  //      title: 'Predective Analytics'
+  //    },
+  //    {
+  //      href: '/landing-page7',
+  //      title: 'Sentiment Analysis'
+  //    },
+  //    {
+  //      href: '/landing-page8',
+  //      title: 'User Behaviour Analysis'
+  //    },
+  //    {
+  //      href: '/landing-page9',
+  //      title: 'Data Scientist Profile'
+  //    },
+  //    {
+  //      href: '/landing-page10',
+  //      title: 'Data Visualization Service'
+  //    },
+  //    {
+  //      href: '/landing-page11',
+  //      title: 'Analytics In Marketing'
+  //    }
+  //  ]
+  //},
+
+  //{
+  //  href: '/blog',
+  //  title: 'Blog',
+  //  submenu: [
+  //    {
+  //      href: '/blog',
+  //      title: 'Blog'
+  //    },
+  //    {
+  //      href: '#blog-grid',
+  //      title: 'Blog Grid',
+  //      submenu: [
+  //        {
+  //          href: '/blog/blog-one-column-grid',
+  //          title: 'One Column Blog'
+  //        },
+  //        {
+  //          href: '/blog/blog-two-column-grid',
+  //          title: 'Two Column Blog'
+  //        },
+  //        {
+  //          href: '/blog/blog-three-column-grid',
+  //          title: 'Three Column Blog',
+  //        }
+  //      ]
+  //    },
+  //    {
+  //      href: '/blog/blog-right-sidebar-grid-one',
+  //      title: 'Right SideBar Grid',
+  //      submenu: [
+  //        {
+  //          href: '/blog/blog-right-sidebar-grid-one',
+  //          title: 'Right Sidebar Grid 1'
+  //        },
+  //        {
+  //          href: '/blog/blog-right-sidebar-grid-two',
+  //          title: 'Right Sidebar Grid 2'
+  //        }
+  //      ]
+  //    },
+  //    {
+  //      href: '/blog/blog-left-sidebar-grid-one',
+  //      title: 'Left SideBar Grid',
+  //      submenu: [
+  //        {
+  //          href: '/blog/blog-left-sidebar-grid-one',
+  //          title: 'Left Sidebar Grid 1'
+  //        },
+  //        {
+  //          href: '/blog/blog-left-sidebar-grid-two',
+  //          title: 'Left Sidebar Grid 1'
+  //        }
+  //      ]
+  //    },
+  //    {
+  //      href: '/blog/blog-details',
+  //      title: 'Blog Details'
+  //    }
+  //  ]
+  //},
+
+  //-----------------------------------------------------------
+
+  //{
+  //  href: '/about-us',
+  //  title: 'About Us ',
+  //  submenu: [
+  //    {
+  //      href: '/about-us',
+  //      title: 'About Us One'
+  //    },
+  //    {
+  //      href: '/about-us/about-us-two',
+  //      title: 'About Us Two'
+  //    }
+  //  ]
+  //},
+  //  ---------------------------------------------------------
+  
+  //  ---------------------------------------------------------
+  //{
+  //  href: '/pages/our-team',
+  //  title: 'Pages',
+  //  submenu: [
+  //    {
+  //      href: '/pages/our-team',
+  //      title: 'Our Team'
+  //    },
+  //    {
+  //      href: '/pages/client',
+  //      title: 'Clients'
+  //    },
+  //    {
+  //      href: '/pages/career',
+  //      title: 'Careers',
+  //    },
+  //    {
+  //      href: '/pages/contact-us',
+  //      title: 'Contact Us',
+  //    }
+  //  ]
+  //}
 ];
 
 const servicesMenu =  [
