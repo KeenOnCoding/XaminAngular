04fccad38ba645b5e3f8ba22c241d807e409d917
Sergei <ryabushenko.serhiy@gmail.com>
2024-06-13 10:49:14 +0300

    devops

--- src/app/landing-page5/components/tips-trick/tips-trick.component.ts
+++ src/app/landing-page5/components/tips-trick/tips-trick.component.ts
@@ -8,13 +8,35 @@ export class TipsTrickComponent implements OnInit {
 
   titleSectionProp1: any = {
     class: 'text-left',
-    subTitle: 'Get tips & tricks on how to skyrocket your sales.',
+    subTitle: 'Continous Integration & Continous Deployment',
     description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
   };
 
   titleSectionProp2: any = {
     class: 'text-left',
-    subTitle: 'Get tips & tricks on how to skyrocket your sales.',
+    subTitle: 'Infrastructure as a Code',
+    description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
+  };
+  titleSectionProp3: any = {
+    class: 'text-left',
+    subTitle: 'Continous Monitoring',
+    description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
+  };
+
+  titleSectionProp4: any = {
+    class: 'text-left',
+    subTitle: 'Continous Security',
+    description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
+  };
+  titleSectionProp5: any = {
+    class: 'text-left',
+    subTitle: 'Kubernetes',
+    description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
+  };
+
+  titleSectionProp6: any = {
+    class: 'text-left',
+    subTitle: 'Microservices',
     description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
   };
 
