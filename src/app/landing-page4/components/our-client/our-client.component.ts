348a040e9044279848988423eaf9580ed6a52a9f
Sergei <ryabushenko.serhiy@gmail.com>
2024-06-20 10:39:30 +0300

    devops_2

--- src/app/landing-page4/components/our-client/our-client.component.ts
+++ src/app/landing-page4/components/our-client/our-client.component.ts
@@ -6,18 +6,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OurClientComponent implements OnInit {
 
+  titleSectionProp: any = {
+    class: 'text-center',
+   // title: 'FEATURES',
+    subTitle: ' Benefits'
+  };
+
   List1: any[] = [
-    { title: 'Microsoft Partner of the Year Award', image: './assets/images/awards/1.png' },
-    { title: 'Global Sourcing Awards 2018 Finalist', image: './assets/images/awards/2.png' },
-    { title: 'Gold Winner of 2013 Horizon Awards', image: './assets/images/awards/3.png' },
-    { title: 'European Software Testing Awards Finalist', image: './assets/images/awards/4.png' }
+    {
+      title: 'Scalability',
+      subtitle: 'Companies with changing demand for their services use cloud-based infrastructure to avoid unnecessary investment in additional equipment.',
+      image: './assets/images/services/01.png'
+    },
+    {
+      title: 'Market agility & speed',
+      subtitle: 'Cloud migration services empower businesses to swiftly adapt and innovate to cater to their customers’ evolving needs and introduce new services.',
+      image: './assets/images/services/02.png'
+    }
   ];
 
   List2: any[] = [
-    { title: 'Microsoft Partner of the Year Award', image: './assets/images/awards/5.png' },
-    { title: 'Global Sourcing Awards 2018 Finalist', image: './assets/images/awards/1.png' },
-    { title: 'Gold Winner of 2013 Horizon Awards', image: './assets/images/awards/2.png' }
-  ];
+    {
+      title: 'Reduced costs',
+      subtitle: 'Cloud infrastructure enables businesses to transit their IT spending from capital costs to operating expenditures, saving considerable money.',
+      image: './assets/images/services/04.png'
+    },
+    {
+      title: 'Business continuity & disaster recovery',
+      subtitle: 'Cloud Service Providers guarantee that key systems and data remain accessible in the case of an interruption or outage.',
+      image: './assets/images/services/05.png'
+    }
+  ]
+
+  //List1: any[] = [
+  //  { title: 'Microsoft Partner of the Year Award', image: './assets/images/awards/1.png' },
+  //  { title: 'Global Sourcing Awards 2018 Finalist', image: './assets/images/awards/2.png' },
+  //  { title: 'Gold Winner of 2013 Horizon Awards', image: './assets/images/awards/3.png' },
+  //  { title: 'European Software Testing Awards Finalist', image: './assets/images/awards/4.png' }
+  //];
+
+  //List2: any[] = [
+  //  { title: 'Microsoft Partner of the Year Award', image: './assets/images/awards/5.png' },
+  //  { title: 'Global Sourcing Awards 2018 Finalist', image: './assets/images/awards/1.png' },
+  //  { title: 'Gold Winner of 2013 Horizon Awards', image: './assets/images/awards/2.png' }
+  //];
 
   constructor() { }
 
--- src/app/landing-page5/components/tips-trick/tips-trick.component.ts
+++ src/app/landing-page5/components/tips-trick/tips-trick.component.ts
@@ -6,39 +6,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TipsTrickComponent implements OnInit {
 
-  titleSectionProp1: any = {
-    class: 'text-left',
-    subTitle: 'Continous Integration & Continous Deployment',
-    description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
-  };
+  //titleSectionProp1: any = {
+  //  class: 'text-left',
+  //  subTitle: 'Continous Integration & Continous Deployment',
+  //  description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
+  //};
 
   titleSectionProp2: any = {
     class: 'text-left',
     subTitle: 'Infrastructure as a Code',
     description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
   };
-  titleSectionProp3: any = {
-    class: 'text-left',
-    subTitle: 'Continous Monitoring',
-    description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
-  };
+  //titleSectionProp3: any = {
+  //  class: 'text-left',
+  //  subTitle: 'Continous Monitoring',
+  //  description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
+  //};
 
-  titleSectionProp4: any = {
-    class: 'text-left',
-    subTitle: 'Continous Security',
-    description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
-  };
-  titleSectionProp5: any = {
-    class: 'text-left',
-    subTitle: 'Kubernetes',
-    description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
-  };
+  //titleSectionProp4: any = {
+  //  class: 'text-left',
+  //  subTitle: 'Continous Security',
+  //  description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
+  //};
+  //titleSectionProp5: any = {
+  //  class: 'text-left',
+  //  subTitle: 'Kubernetes',
+  //  description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
+  //};
 
-  titleSectionProp6: any = {
-    class: 'text-left',
-    subTitle: 'Microservices',
-    description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
-  };
+  //titleSectionProp6: any = {
+  //  class: 'text-left',
+  //  subTitle: 'Microservices',
+  //  description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
+  //};
 
   List1: any[] = ['Boost SEO ranking', 'Social Sharing', 'Marketing', 'Retention', 'Visual Reviews', 'Reviews Generation'];
   List2: any[] = ['Boost SEO ranking', 'Social Sharing', 'Marketing', 'Retention', 'Visual Reviews', 'Reviews Generation'];
