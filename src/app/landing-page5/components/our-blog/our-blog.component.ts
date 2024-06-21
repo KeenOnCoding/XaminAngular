6f5faf0c6b72e0aee3c712c2d595d59b8d54199f
Sergei <ryabushenko.serhiy@gmail.com>
2024-06-21 10:39:03 +0300

    DevOps_3

--- src/app/landing-page5/components/our-blog/our-blog.component.ts
+++ src/app/landing-page5/components/our-blog/our-blog.component.ts
@@ -8,41 +8,94 @@ export class OurBlogComponent implements OnInit {
 
   titleSectionProp: any = {
     class: 'text-center',
-    title: 'BLOG',
-    subTitle: 'Recent Blog',
-    desc:'It is a long established fact that a reader will be distracted'
+    title: 'our Services',
+    subTitle: 'Explore Our Data Services'
   };
 
-  List: any[] = [
+  navList: any[] = [
     {
-      img: './assets/images/about-us/02.png',
-      name:  'HTML',
-      time: 'September 26, 2019 ',
-      title: 'Build Construction',
-      desc: '“They bring to you a host of beautifully created infographics that contain the latest digital marketing ',
+      id: 'tab-software-development',
+      title: 'CI/CD',
+      sub_title: "Continuous Integration & Continuous Deployment",
+      desc: "Continuous Integration (CI) pipelines automatically build and test your code very time it's written to detect failures and bugs in the earliest stages. Continuous Deployment (CD) pipelines deploy the code in various environmentsto gain early feedback every time the product is built and passes the testing procedure.",
+      active: true,
+      chkList: ['Advance Advisory Team', 'Professional Consulting Services', '24/7 Support Help Center', 'Customer Service & Operations', 'There are many variations of passages.']
     },
     {
-      img: './assets/images/about-us/03.png',
-      name:  'Design',
-      time: 'September 26, 2019 ',
-      title: 'Life Lack Meaning',
-      desc: 'They bring to you a host of beautifully created infographics that contain the latest digital marketing.',
+      id: 'tab-application-services',
+      title: 'IaC',
+      sub_title: "Choose Your Application Service",
+      desc: "Tame your big data through robust solutions that empower data collection, storage,processing and analysis.",
+      chkList: ['Advance Advisory Team', 'Professional Consulting Services', '24/7 Support Help Center', 'Customer Service & Operations', 'There are many variations of passages.']
     },
     {
-      img: './assets/images/about-us/04.png',
-      name:  'HTML',
-      time: 'September 26, 2019 ',
-      title: 'Construction industry',
-      desc: 'They bring to you a host of beautifully created infographics that contain the latest digital marketing',
+      id: 'tab-data-analytics',
+      title: 'Microservices',
+      sub_title: "Choose Your Data Analytics Service",
+      desc: "Tame your big data through robust solutions that empower data collection, storage,processing and analysis.",
+      chkList: ['Advance Advisory Team', 'Professional Consulting Services', '24/7 Support Help Center', 'Customer Service & Operations', 'There are many variations of passages.']
     },
     {
-      img: './assets/images/about-us/05.png',
-      name:  'CSS',
-      time: 'September 26, 2019 ',
-      title: 'Content Marketing',
-      desc: 'They bring to you a host of beautifully created infographics that contain the latest digital marketing.',
+      id: 'tab-help-desk-services',
+      title: 'Kubernetes',
+      sub_title: "Choose Your Help Desk Service",
+      desc: "Tame your big data through robust solutions that empower data collection, storage,processing and analysis.",
+      chkList: ['Advance Advisory Team', 'Professional Consulting Services', '24/7 Support Help Center', 'Customer Service & Operations', 'There are many variations of passages.']
+    },
+    {
+      id: 'tab-software-development',
+      title: 'Continuous Monitoring',
+      sub_title: "Continuous Integration & Continuous Deployment",
+      desc: "Continuous Integration (CI) pipelines automatically build and test your code very time it's written to detect failures and bugs in the earliest stages. Continuous Deployment (CD) pipelines deploy the code in various environmentsto gain early feedback every time the product is built and passes the testing procedure.",
+      chkList: ['Advance Advisory Team', 'Professional Consulting Services', '24/7 Support Help Center', 'Customer Service & Operations', 'There are many variations of passages.']
+    },
+    {
+      id: 'tab-application-services',
+      title: 'Continuous Security',
+      sub_title: "Choose Your Application Service",
+      desc: "Tame your big data through robust solutions that empower data collection, storage,processing and analysis.",
+      chkList: ['Advance Advisory Team', 'Professional Consulting Services', '24/7 Support Help Center', 'Customer Service & Operations', 'There are many variations of passages.']
     }
   ];
+
+
+  //titleSectionProp: any = {
+  //  class: 'text-center',
+  //  title: 'BLOG',
+  //  subTitle: 'Recent Blog',
+  //  desc:'It is a long established fact that a reader will be distracted'
+  //};
+
+  //List: any[] = [
+  //  {
+  //    img: './assets/images/about-us/02.png',
+  //    name:  'HTML',
+  //    time: 'September 26, 2019 ',
+  //    title: 'Build Construction',
+  //    desc: '“They bring to you a host of beautifully created infographics that contain the latest digital marketing ',
+  //  },
+  //  {
+  //    img: './assets/images/about-us/03.png',
+  //    name:  'Design',
+  //    time: 'September 26, 2019 ',
+  //    title: 'Life Lack Meaning',
+  //    desc: 'They bring to you a host of beautifully created infographics that contain the latest digital marketing.',
+  //  },
+  //  {
+  //    img: './assets/images/about-us/04.png',
+  //    name:  'HTML',
+  //    time: 'September 26, 2019 ',
+  //    title: 'Construction industry',
+  //    desc: 'They bring to you a host of beautifully created infographics that contain the latest digital marketing',
+  //  },
+  //  {
+  //    img: './assets/images/about-us/05.png',
+  //    name:  'CSS',
+  //    time: 'September 26, 2019 ',
+  //    title: 'Content Marketing',
+  //    desc: 'They bring to you a host of beautifully created infographics that contain the latest digital marketing.',
+  //  }
+  //];
 
   constructor() { }
 
--- src/app/landing-page5/components/tips-trick/tips-trick.component.ts
+++ src/app/landing-page5/components/tips-trick/tips-trick.component.ts
@@ -6,39 +6,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TipsTrickComponent implements OnInit {
 
-  //titleSectionProp1: any = {
-  //  class: 'text-left',
-  //  subTitle: 'Continous Integration & Continous Deployment',
-  //  description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
-  //};
 
-  titleSectionProp2: any = {
+  titleSectionProp: any = {
     class: 'text-left',
-    subTitle: 'Infrastructure as a Code',
-    description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
+    title: '',
+    subTitle: 'Revolutionize Your Software Delivery with  DevOps',
+    description: 'We provide a comprehensive suite of DevOps services from start to finish, enabling you to improve efficiency, automate workflows, eliminate human errors during development and operations processes, and accelerate your time-to-market. Our top Cloud experts will create well- designed CI/CD pipelines for your business using the best DevOps solutions, including Kubernetes, Jenkins, Terraform, Docker, and more.'
   };
-  //titleSectionProp3: any = {
-  //  class: 'text-left',
-  //  subTitle: 'Continous Monitoring',
-  //  description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
-  //};
 
-  //titleSectionProp4: any = {
-  //  class: 'text-left',
-  //  subTitle: 'Continous Security',
-  //  description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
-  //};
-  //titleSectionProp5: any = {
-  //  class: 'text-left',
-  //  subTitle: 'Kubernetes',
-  //  description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
-  //};
-
-  //titleSectionProp6: any = {
-  //  class: 'text-left',
-  //  subTitle: 'Microservices',
-  //  description: 'Faff about only a quid blower I don’t want no agro bleeding chimney pot burke tosser cras nice one boot fanny.! '
-  //};
+  List: any[] = [
+    {
+      //title: 'Data Visualization Practices to Improve',
+     // icon: 'fa fa-database',
+      //desc: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+    },
+    {
+      //title: 'Focused on Actionable Insights',
+      //icon: 'fa fa-align-left',
+      //desc: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+    }
+  ]
 
   List1: any[] = ['Boost SEO ranking', 'Social Sharing', 'Marketing', 'Retention', 'Visual Reviews', 'Reviews Generation'];
   List2: any[] = ['Boost SEO ranking', 'Social Sharing', 'Marketing', 'Retention', 'Visual Reviews', 'Reviews Generation'];
