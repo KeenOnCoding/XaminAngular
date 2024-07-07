bbf5d27925ba7d16d202c831bf10b8ff4b3b8fb0
Sergei <ryabushenko.serhiy@gmail.com>
2024-07-07 10:07:09 +0300

    images_expertise

--- src/app/landing-page1/components/section-three/section-three.component.ts
+++ src/app/landing-page1/components/section-three/section-three.component.ts
@@ -7,70 +7,73 @@ import { Component, OnInit } from '@angular/core';
 export class SectionThreeComponent implements OnInit {
 
   titleSectionProp: any = {
-    class: 'text-center',
+    class: 'text-center text-white',
     //title: 'How it’s work',
     subTitle: 'Our Expertise'
   };
 
   circle1Data: any = {
-    img: '01.png',
+    img: 'cicd.png',
     title: 'CI\\CD',
-    description: 'Reduce your time to market by ensuring predictable software deployment every time',
+    /*description: 'Reduce your time to market by ensuring predictable software deployment every time',*/
     delay: '0.6s',
     positionClass: 'right',
-    type:'consult-services'
+  /*  type:'consult-services'*/
 
   };
 
   circle2Data: any = {
-    class: 'mt-4 mt-md-0',
-    img: '02.png',
-    title: 'AI/ML',
-    description: 'ML cloud journey from the beginning to advanced pipelines',
+    class: 'mt-4 mt-md-4',
+    img: 'artificial-intelligence.png',
+    title: 'Machine Learning',
+   /* description: 'ML cloud journey from the beginning to advanced pipelines',*/
     delay: '1s',
     positionClass: 'right',
-    type:'consult-services'
+    /*type:'consult-services'*/
 
   };
 
   circle3Data: any = {
-    class: 'mt-4 mt-md-0',
-    img: '03.png',
-    title: 'GenAI',
-    description: 'Accelerate the power of LLMs, implement guardrails & evaluate models for your RAG to leverage business insights',
+    class: 'mt-4 mt-md-4',
+    img: 'generative-image.png',
+    title: 'Generative AI',
+    /*description: 'Accelerate the power of LLMs, implement guardrails & evaluate models for your RAG to leverage business insights',*/
     delay: '1.4s',
-    type:'consult-services'
+    /*type:'consult-services'*/
 
   };
 
   circle4Data: any = {
-    class: 'mt-4 mt-md-0',
-    img: '01.png',
+    class: 'mt-4 mt-md-4',
+    img: 'security.png',
     title: 'Security',
-    description: 'Safeguard your business by implementing best practices',
+    /*description: 'Safeguard your business by implementing best practices',*/
     delay: '1s',
     positionClass: 'right',
-    type: 'consult-services'
+    /*    type: 'consult-services'*/
+    type: 'mt-4'
 
   };
 
   circle5Data: any = {
-    class: 'mt-4 mt-md-0',
-    img: '02.png',
-    title: 'Application Lifecycle Management',
-    description: 'Run on an optimized Kubernetes workload',
+    class: 'mt-4 mt-md-4',
+    img: 'ALM.png',
+    title: 'App Lifecycle Management',
+    /*description: 'Run on an optimized Kubernetes workload',*/
     delay: '1.4s',
-    type: 'consult-services'
+    /*    type: 'consult-services'*/
+    type: 'mt-4'
 
   };
 
   circle6Data: any = {
-    class: 'mt-4 mt-md-0',
-    img: '03.png',
+    class: 'mt-4 mt-md-4',
+    img: 'SRE.png',
     title: 'Site Reliability Engineering',
-    description: 'Accelerate the power of LLMs, implement guardrails & evaluate models for your RAG to leverage business insights',
+    /*description: 'Accelerate the power of LLMs, implement guardrails & evaluate models for your RAG to leverage business insights',*/
     delay: '1.4s',
-    type: 'consult-services'
+    /*    type: 'consult-services'*/
+    type: 'mt-4'
 
   };
 
