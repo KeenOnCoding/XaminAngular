1f78d400ae2bc3def0d2357a37241d8820c542e6
Sergei <ryabushenko.serhiy@gmail.com>
2024-07-13 10:43:51 +0300

    partners

--- src/app/home-page/components/section-two/section-two.component.ts
+++ src/app/home-page/components/section-two/section-two.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class SectionVerticalOneComponent implements OnInit {
 
   titleSectionProp: any = {
-    class: 'text-center',
-   // title: 'About us',
-    subTitle: 'Who We Are',
+    class: 'text-justify text-white',
+    // title: 'About us',
+    //subTitle: 'Who We Are',
     // tslint:disable-next-line:max-line-length
     description: 'As DevOps and cloud best practices experts, we empower customer-tailored software release pipelines for accelerated time to market.Our 100% customized solutions include Cloud Migration, DevOps, Well- Architected Framework Review, FinOps, Site Reliability Engineering(SRE), DevSecOps, Application Lifecycle Management(ALM), and Network Operations Center(NOC) services.'
   };
--- src/app/well-architected/components/our-service/our-service.component.ts
+++ src/app/well-architected/components/our-service/our-service.component.ts
@@ -11,7 +11,33 @@ export class OurServiceComponent implements OnInit {
     subTitle: 'How It Works',
     description: 'We understand the vital alignment of cloud infrastructure with your business objectives. As your trusted partner for AWS Well-Architected Reviews, we offer a comprehensive approach to assess, optimize, and align your cloud architecture with AWS Well-Architected Framework.'
   };
-
+  List: any[] = [
+    {
+      title: 'Initial Meeting',
+      subtitle: 'In our introductory meeting, we’ll provide you with an outline of the review process. We highlight the industry significance program, share pertinent case studies, discuss primary objectives, review technical documentation for each pillar, and outline solution deliverables',
+      img: './assets/images/about-us/1.png',
+      time: '30 min - 1 hour',
+    },
+    {
+      title: 'Review',
+      subtitle: 'The core of the review involves a thorough assessment of your current cloud architecture and infrastructure. Our AWS-certified experts conduct interviews with your technical and business teams to validate alignment with the AWS Well-Architected Framework',
+      img: './assets/images/about-us/2.png',
+      time: '30 min - 1 hour'
+    },
+    {
+      title: 'Presentation',
+      subtitle: 'Following the review, we submit a comprehensive report outlining identified risks in each of the six pillars. This report serves as a valuable reference, highlighting areas requiring attention and improvement. We also present a plan for remediating noncompliant resources setting out clear priorities and tools to address identified risks.',
+      img: './assets/images/about-us/3.png',
+      time: '2 hours'
+    },
+    {
+      title: 'Remediation Plan',
+      subtitle: 'Our team, including a project manager, chief solutions architect, and certified cloud engineers, collaborates closely with your team to implement necessary improvements',
+      img: './assets/images/about-us/4.png',
+      time: '30 min - 1 hour'
+    }
+    ]
+  /*
   List: any[] = [
     {
       class: 'bg-gray',
@@ -42,7 +68,7 @@ export class OurServiceComponent implements OnInit {
     },
   ];
 
-  /*
+  
   titleSectionProp: any = {
     class: 'text-center',
     title: 'Our Services',
